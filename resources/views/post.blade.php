4a9c82ad87ab2042c36beec95f80bf36c94c705f
altafin <marbr72@hotmail.com>
2023-11-26 21:48:19 -0300

    second commit

---
+++ resources/views/post.blade.php
@@ -0,0 +1,49 @@
+{{--<x-app-layout>--}}
+{{--    <x-slot name="header">--}}
+{{--        <h2 class="font-semibold text-xl text-gray-800 leading-tight">--}}
+{{--            {{ __('Posts') }}--}}
+{{--        </h2>--}}
+{{--    </x-slot>--}}
+
+{{--    <div class="py-12">--}}
+{{--        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">--}}
+{{--            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">--}}
+{{--                <div class="p-6 text-gray-900">--}}
+{{--                    @livewire('posts')--}}
+{{--                </div>--}}
+{{--            </div>--}}
+{{--        </div>--}}
+{{--    </div>--}}
+{{--</x-app-layout>--}}
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Laravel Livewire CRUD - ItSolutionStuff.com</title>
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" rel="stylesheet">
+    @livewireStyles
+</head>
+<body>
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">
+                    <h2>Laravel Livewire CRUD - ItSolutionStuff.com</h2>
+                </div>
+                <div class="card-body">
+                    @if (session()->has('message'))
+                        <div class="alert alert-success">
+                            {{ session('message') }}
+                        </div>
+                    @endif
+                    @livewire('posts')
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@livewireScripts
+</body>
+</html>
