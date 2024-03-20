e33ce928adef0fcadc34bac603d8cee30eb0625c
altafin <altafin@uel.br>
2024-03-20 12:20:06 -0300

    Ajuste no layout da listagem de pessoas.

--- resources/views/admin/person/index.blade.php
+++ resources/views/admin/person/index.blade.php
@@ -31,20 +31,23 @@
                     <a href="{{ route(strtolower($model) . '.create') }}" class="btn-sm btn-outline-info float-right" style="border: 2px solid white; border-radius: 5px;">Novo registro</a>
                 </div>
                 <div class="card-body table-responsive p-0">
-                    <table id="example" class="table table-hover text-nowrap" style="width:100%">
+                    <table id="person" class="table table-hover text-nowrap" style="width:100%">
                         <thead>
                             <tr>
-                                <th>Ação</th>
                                 <th>Nome</th>
-                                <th>Tipo</th>
+                                <th  style="text-align: left;">Tipo</th>
+                                <th style="text-align: center;">Ação</th>
                             </tr>
                         </thead>
                         <tbody>
                         @foreach($people->items() as $person)
                             <tr>
-                                <td><a href="{{ route(strtolower($model) . '.edit', $person->id) }}">edit</a></td>
                                 <td>{{ $person->name }}</td>
-                                <td>{{ $person->type == 'J' ? 'Jurídica' : 'Física'  }}</td>
+                                <td style="width: 150px; text-align: left;">{{ $person->type == 'J' ? 'Jurídica' : 'Física'  }}</td>
+                                <td style="width: 10px; text-align: right;">
+                                    <a href="{{ route(strtolower($model) . '.edit', $person->id) }}" class="btn btn-sm btn-warning" style="margin-right: 10px;"><i class="fa fa-pen" aria-hidden="true"></i></a>
+                                    <a href="" class="btn btn-sm btn-danger"><i class="fa fa-trash" aria-hidden="true"></i></a>
+                                </td>
                             </tr>
                         @endforeach
                         </tbody>
@@ -59,10 +62,6 @@
                             </tr>
                         </tfoot>
                     </table>
-{{--                    <x-pagination--}}
-{{--                        :paginator="$people"--}}
-{{--                        :appends="$filters"--}}
-{{--                    />--}}
                 </div>
             </div>
         </div>
@@ -70,6 +69,8 @@
 @stop
 
 @section('css')
+
+
 @stop
 
 @section('js')
